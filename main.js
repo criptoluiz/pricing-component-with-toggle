776e001447167018b0be2bc35fd3b69f3031d182
luiz <luiz.pb@cryptolab.net>
2020-05-22 19:51:29 -0300

    toggle switch

---
+++ main.js
@@ -0,0 +1,4 @@
+const toggleSwitch = document.getElementById("");
+const basicPrice = document.getElementById();
+const professionalPrice = document.getElementById();
+const masterPrice = document.getElementById();
